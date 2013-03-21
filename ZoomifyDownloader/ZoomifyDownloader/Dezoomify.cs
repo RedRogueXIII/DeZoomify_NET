dfac502b940078492b34c56dc3e527a3056eec02
Michael Cecconet <red_rogue_xiii@hotmail.com>
2013-03-21 05:50:38 -0400

    Initial Functionality Commit
    Only works by using the direct Target URL ( the folder path that the
    zoomify image refers to - NOT the actual URL of the page that the
    zoomify viewer is on) at this iteration.  Batch handling of direct
    target URLs is the next planned update, and then web viewed URL
    downloading after.

---
+++ ZoomifyDownloader/ZoomifyDownloader/Dezoomify.cs
@@ -0,0 +1,205 @@
+﻿/*
+ * Based on the Dezoomify javascript by lovasoa. 
+ * https://gist.github.com/lovasoa/770310 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Drawing;
+
+namespace ZoomifyDownloader
+{
+    class Dezoomify
+    {
+
+        public delegate void progressUpdate(int value);
+
+        public static Stream DownloadStream( Stream outputStream, string url, string contentType )
+        {
+            WebRequest request = WebRequest.Create(url);
+            request.ContentType = contentType;
+            request.Method = "GET";
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            // Get the stream containing content returned by the server.
+            outputStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            //StreamReader reader = new StreamReader(outputStream);
+            // Read the content.
+            //string responseFromServer = reader.ReadToEnd();
+            // Display the content.
+            //Console.WriteLine(responseFromServer);
+            // Clean up the streams and the response.
+            //reader.Close();
+            response.Close();
+            return outputStream;
+        }
+
+        public static Bitmap Download(string url, progressUpdate updater) 
+        {
+            if (url != null && url.Length > 0)
+            {
+                string infoXMLFile = url;
+                //Zoomify Images are stored in pieces with a control file.
+                //Determine if the path is already linking to the proper file or not.
+                if (!infoXMLFile.Contains("ImageProperties.xml"))
+                {
+                    infoXMLFile += "/ImageProperties.xml";
+                }
+                Console.WriteLine("Hello, Download operation starting on: " + infoXMLFile);
+                url = Uri.EscapeUriString(url);
+                infoXMLFile = Uri.EscapeUriString(infoXMLFile);
+                Console.WriteLine("String escape: " + infoXMLFile);
+                // Start download of the image information file.
+                WebRequest request = WebRequest.Create(infoXMLFile);
+                request.ContentType = "text/xml";
+                request.Method = "GET";
+                // If required by the server, set the credentials.
+                request.Credentials = CredentialCache.DefaultCredentials;
+                // Get the response.
+                WebResponse response = request.GetResponse();
+                // Display the status.
+                Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+                // Clean up the streams and the response.
+                reader.Close();
+                response.Close();
+
+                //Found information file, continue to determine information about images.
+                XmlDocument xhr = new XmlDocument();
+                xhr.LoadXml(responseFromServer);
+                XmlNode path = xhr.FirstChild.Attributes.GetNamedItem("path");
+                XmlNodeList temp = xhr.GetElementsByTagName("IMAGE_PROPERTIES");
+                if (temp.Count > 0)
+                {
+                    XmlNode width = temp[0].Attributes.GetNamedItem("WIDTH");
+                    XmlNode height = temp[0].Attributes.GetNamedItem("HEIGHT");
+                    XmlNode tilesize = temp[0].Attributes.GetNamedItem("TILESIZE");
+                    XmlNode numtiles = temp[0].Attributes.GetNamedItem("NUMTILES");
+                    return drawImage(url, temp[0], updater);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No URL specified.");                
+            }
+            return null;
+        }
+
+
+        static void remove(string el)
+        {
+        }
+        // Overwrites a target image pixels with another image at location x,y, using the new image's resolution.
+        public static Bitmap overlayImage(Bitmap target, Bitmap newImage, int x, int y)
+        {
+            for (int i = 0; i < newImage.Width; i++)
+            {
+                for (int j = 0; j < newImage.Height; j++)
+                {
+                    target.SetPixel( x+i, y+j, newImage.GetPixel(i,j) );
+                }
+            }
+            return target;
+        }
+        static void loadEnd()
+        {
+        }
+        static void changeSize()
+        {
+        }
+        static double findZoom(double size) 
+        {
+            //Fonction de BG
+            return Math.Floor(Math.Log(size) / Math.Log(2) - 7);
+        }
+        static int findZoom(int size)
+        {
+            return System.Convert.ToInt32( Math.Floor( Math.Log(System.Convert.ToDouble(size)) / Math.Log(2) - 7 ) );
+        }
+        static Bitmap drawImage(string path, XmlNode information, progressUpdate loadingInformation)
+        {
+            int width = System.Convert.ToInt32(information.Attributes.GetNamedItem("WIDTH").Value);
+            int height = System.Convert.ToInt32(information.Attributes.GetNamedItem("HEIGHT").Value);
+            int tileSize = System.Convert.ToInt32( information.Attributes.GetNamedItem("TILESIZE").Value );
+            int numTiles = System.Convert.ToInt32( information.Attributes.GetNamedItem("NUMTILES").Value );
+
+           loadingInformation(0);
+
+            //Create a canvas ( image array )
+            Bitmap output = new Bitmap(width, height, System.Drawing.Imaging. PixelFormat.Format24bppRgb);
+            int zoom = (width > height) ? findZoom(width) : findZoom(height);
+            Console.WriteLine("Maximum Zoom Level: "+zoom);
+
+            int nbrTilesX = (int) Math.Ceiling( (float)width / (float)tileSize );
+            int nbrTilesY = (int) Math.Ceiling( (float)height / (float)tileSize );
+            int loaded = 0;
+            int totalTiles = nbrTilesX * nbrTilesY;
+            int skippedTiles = numTiles - totalTiles;
+
+            int tileGroup = (int) Math.Floor( (float)skippedTiles / 256 ); // Original script divides by 256. Perhaps it refers to the tile size?
+            int tileGroupCounter = (int)((float)skippedTiles % 256); // Original script divides by 256. Perhaps it refers to the tile size?
+
+            int x, y;
+            for (y = 0; y < nbrTilesY; y++)
+            {
+                for (x = 0; x < nbrTilesX; x++)
+                {
+
+                    if (tileGroupCounter >= 256)
+                    {
+                        tileGroup++;
+                        tileGroupCounter = 0;
+                    }
+
+                    tileGroupCounter++;
+
+                    string url = path + "/TileGroup" + tileGroup + "/" + zoom + "-" + x + "-" + y + ".jpg";                    
+                    Console.WriteLine("GET: "+url);
+                    //Add a tile image to the canvas.
+
+                    //Method 0
+                    //Stream tileDownload = null;
+                    //tileDownload = DownloadStream(tileDownload, url, "image/jpeg");
+                    //if (tileDownload.CanRead)
+                    //{
+                    //    Bitmap currentTile = new Bitmap(tileDownload);
+                    //    output = overlayImage(output, currentTile, x * tileSize, y * tileSize);                        
+                    //}
+                    //tileDownload.Close();
+
+                    //Method 1
+                    WebRequest requestPic = WebRequest.Create(url);
+                    WebResponse responsePic = requestPic.GetResponse();
+                    Image webImage = Image.FromStream(responsePic.GetResponseStream()); // Error
+                    if (webImage != null)
+                    {
+                        Bitmap currentTile = new Bitmap(webImage);
+                        output = overlayImage(output, currentTile, x * tileSize, y * tileSize);     
+                    }
+                    responsePic.Close();
+                    loadingInformation((int)((float)loaded / (float)numTiles) * 100);
+                    loaded++;
+                }
+            }
+            //Return image.
+            return output;
+        }
+    }
+}
---
+++ ZoomifyDownloader/ZoomifyDownloader/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZoomifyDownloader
+{
+    public partial class Form1 : Form
+    {
+        protected ImageFormat outFormat = ImageFormat.Png;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text = System.Environment.GetFolderPath(Environment.SpecialFolder.MyPictures, Environment.SpecialFolderOption.Create);
+        }
+
+        private void button1_click(object sender, EventArgs e)
+        {
+
+            Bitmap download = Dezoomify.Download(textBox1.Text, SetDownloadProgress);
+            if( download != null ) {
+                //Check for if file with name already exists.
+                //Allow user to specify what image format to ouput.
+                //Allow user to set options of output image format.
+                //Save frequent and last used settings.
+                //Allow batch downloading and renaming.
+                //Allow renaming masks.
+                //Optional open folder upon finish.                
+                download.Save(textBox2.Text + "\\" + textBox3.Text+"." + outFormat.ToString().ToLower(), outFormat);                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("File will be saved to: " + textBox2.Text + "\\" + textBox3.Text + "." + outFormat.ToString().ToLower());
+            System.Diagnostics.Process.Start("explorer.exe", @textBox2.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        // Progress Bar - Requires multithreading and adds more complexity than I care to get into at the moment.
+        public void SetDownloadProgress(int value)
+        {
+            progressBar1.Value = value;
+        }
+
+        private void OpenFolderBrowserDialog(object sender, MouseEventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (textBox2.Text.Length > 0)
+            {
+                folderBrowserDialog1.SelectedPath = textBox2.Text;
+            }
+            folderBrowserDialog1.Description = "Please select a valid folder for your downloads.";
+            folderBrowserDialog1.ShowDialog();
+            Console.WriteLine("Test1");
+            if (folderBrowserDialog1.SelectedPath.Length > 0)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+            else
+            {
+                Console.WriteLine("No Folder was selected.");
+            }
+
+            Console.WriteLine(sender.ToString());
+        }
+        private void OpenFileFolderBrowserDialog(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog folderBrowserDialog1 = new OpenFileDialog();
+            folderBrowserDialog1.Filter = "";
+            folderBrowserDialog1.Title = "Please select a valid folder for your downloads.";
+            folderBrowserDialog1.ShowDialog();
+            textBox2.Text = folderBrowserDialog1.FileName;
+            Console.WriteLine("Test2");
+        }
+
+        private void OpenFileSaveDialog(object sender, MouseEventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Jpeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Portable Network Graphic Image|*.png";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.InitialDirectory = textBox2.Text;
+            saveFileDialog1.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog1.FileName != "")
+            {
+                textBox3.Text = saveFileDialog1.FileName;
+            } 
+        }
+    }
+}
